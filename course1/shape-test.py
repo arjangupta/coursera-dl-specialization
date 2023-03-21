204b93141c3ac5bc7814b3fa27c421576947fbca
Arjan Gupta <arjan.gupta@lindsay.com>
2023-03-21 13:15:41 -0500

    Add a broadcasting example

--- course1/shape-test.py
+++ course1/shape-test.py
@@ -16,3 +16,10 @@ print(f'Shape of b is {b.shape}')
 
 q7 = np.array([[2,1],[1,3]])
 print(f'Result a q7*q7 is {q7*q7}')
+
+q8_a = np.random.randint(low=0,high=10,size=(4,3))
+print(f'q8_a is {q8_a}')
+q8_b = np.random.randint(low=0,high=10,size=(4,1))
+print(f'q8_b is {q8_b}')
+q8_c = q8_a + q8_b
+print(f'q8_c is {q8_c}')
