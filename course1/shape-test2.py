ba012f75229920cd4f100dc3ad17591af5cd4e8a
Arjan Gupta <arjan.gupta@lindsay.com>
2023-03-21 13:42:02 -0500

    Add q5

--- course1/shape-test2.py
+++ course1/shape-test2.py
@@ -4,3 +4,9 @@ q4_a = np.random.randn(2,3)
 q4_b = np.random.randn(2,1)
 q4_c = q4_a + q4_b
 print(f'q4_c shape is {q4_c.shape}')
+
+q5_a = np.random.randn(4,3)
+q5_b = np.random.randn(3,2)
+# ValueError: operands could not be broadcast together with shapes (4,3) (3,2)
+# q5_c = q5_a*q5_b 
+# print(f'q5_c shape is {q5_c.shape}')
