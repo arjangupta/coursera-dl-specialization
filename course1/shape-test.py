b92aae1cf6340235616f572fb3bf211f64d2422e
Arjan Gupta <arjan.gupta@lindsay.com>
2023-03-21 13:34:31 -0500

    Correct one of the shape tests

--- course1/shape-test.py
+++ course1/shape-test.py
@@ -1,18 +1,21 @@
 import numpy as np
 
-a = np.random.randn(3,3)
-print(f'Shape of a is {a.shape}')
+# a = np.random.randn(3,3)
+# print(f'Shape of a is {a.shape}')
 
-b = np.random.randn(2,1)
-print(f'Shape of b is {b.shape}')
-
+# b = np.random.randn(2,1)
+# print(f'Shape of b is {b.shape}')
 # The following does not work because broadcasting doesn't work that way
 # c = a+b
 # print(f'Shape of c is {c.shape}')
 
-# The following does not work because broadcasting doesn't work that way
-# c = a*b
-# print(f'Shape of c is {c.shape}')
+
+a = np.random.randn(1,3)
+print(f'Shape of a is {a.shape}')
+b = np.random.randn(3,3)
+print(f'Shape of b is {b.shape}')
+c = a*b
+print(f'Shape of c is {c.shape}')
 
 q7 = np.array([[2,1],[1,3]])
 print(f'Result a q7*q7 is {q7*q7}')
