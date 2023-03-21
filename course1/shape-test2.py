facd02d2c68dbd4ead9ac854330d40acf555fe26
Arjan Gupta <arjan.gupta@lindsay.com>
2023-03-21 13:49:50 -0500

    Vectorize q8 2nd attempt

--- course1/shape-test2.py
+++ course1/shape-test2.py
@@ -10,3 +10,15 @@ q5_b = np.random.randn(3,2)
 # ValueError: operands could not be broadcast together with shapes (4,3) (3,2)
 # q5_c = q5_a*q5_b 
 # print(f'q5_c shape is {q5_c.shape}')
+
+q8_a = np.random.randint(low=0,high=10,size=(3,4))
+print(f'q8_a is {q8_a}')
+q8_b = np.random.randint(low=0,high=10,size=(4,1))
+print(f'q8_b is {q8_b}')
+q8_c = np.zeros((3,4), dtype=int)
+for i in range(3):
+    for j in range(4):
+        q8_c[i][j] = q8_a[i][j] + q8_b[j]
+print(f'q8_c is {q8_c}')
+q8_c_vectorized = q8_a + q8_b.T
+print(f'q8_c_vectorized is {q8_c_vectorized}')
